7d55b2b1d92a8d5a3e7cce33d3a786cfe20f19e0
kimsolji <ggg454538@gmail.com>
2024-04-26 14:28:33 +0900

    26-1

---
+++ src/component/game/FaceChat.jsx
@@ -0,0 +1,12 @@
+function FaceChat({onGameParty}) {
+    console.log(onGameParty)
+    return(
+        <div className="faceChatContent">
+            <div className="nickNmContent">{onGameParty.userNickNm}</div>
+            <div className="videoBox"></div>
+        </div>
+    );   
+}
+
+export default FaceChat;4
+
