d11bb505867c317509fb3315d8dd262530f37309
billihazero <billihazero@gmail.com>
2024-04-28 16:57:08 +0900

    24.04.27 커뮤니티 페이지 구현, 게임 파일3개 삭제

--- src/component/boards/CommunityListItem.jsx
+++ src/component/boards/CommunityListItem.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
 function CommunityListItem(props){
+
     const {communityData, onClick} = props;
 
     return(
-        
         <div className="communityItem" onClick={onClick}>
-            <div className="communityNo">{communityData.postNo}</div>
-            <div className="communityTitle">{communityData.title}</div>
-            <div className="communityId">{communityData.userId}</div>
-            <div className="communityWrtnDate">{communityData.wrtnDate}</div>
-            <div className="communityVwCnt">{communityData.vwCnt}</div>
+            <div className="itemNo">{communityData.postNo}</div>
+            <div className="itemTitle">{communityData.title}</div>
+            <div className="itemId">{communityData.userId}</div>
+            <div className="itemWrtnDate">{communityData.wrtnDate}</div>
             <br/><br/>
         </div>
     )
-    
 }
 
 export default CommunityListItem;
