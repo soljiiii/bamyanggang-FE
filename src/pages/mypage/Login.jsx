b4b13afcaa17aaca3dc604455ebbefa6a10d5bf3
geaun0130 <delicate0130@gmail.com>
2024-05-08 17:26:30 +0900

    이미지 로그인, 회원가입 css수정

--- src/pages/mypage/Login.jsx
+++ src/pages/mypage/Login.jsx
@@ -53,22 +53,24 @@ function Login() {
             <form onSubmit={handleSubmit}>
                 <h1>로그인</h1>
                 <div>
-                    <label htmlFor="userId">아이디:</label>
-                    <input
+                    {/* <label htmlFor="userId">아이디</label> */}
+                    <input className='text'
                         type="text"
                         name="userId"
                         id="userId"
+                        placeholder='아이디'
                         value={credentials.userId}
                         onChange={handleChange}
                         required
                     />
                 </div>
                 <div>
-                    <label htmlFor="userPw">비밀번호:</label>
-                    <input
+                    {/* <label htmlFor="userPw">비밀번호</label> */}
+                    <input className='text2'
                         type="password"
                         name="userPw"
                         id="userPw"
+                        placeholder='비밀번호'
                         value={credentials.userPw}
                         onChange={handleChange}
                         required
